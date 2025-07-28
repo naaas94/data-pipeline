e26723c926b2772a0fd83e8cc1bb42e0e2cf9f46
naaas94 <alejandroa.garay.ag@gmail.com>
2025-07-28 14:33:20 -0300

    fixes

--- src/data/dataset_generator.py
+++ src/data/dataset_generator.py
@@ -10,8 +10,10 @@ def process_and_upload_dataset(df, bucket_name='pcc-datasets'):
     # Generate embeddings for the dataset
     embeddings = embedding_generator.generate_privacy_domain_embeddings(df['text'].tolist())
 
-    # Add embeddings to the DataFrame
-    df = df.assign(embeddings=list(embeddings))
+    # Split embeddings into separate columns
+    embedding_columns = [f'embedding_{i}' for i in range(embeddings.shape[1])]
+    embeddings_df = pd.DataFrame(embeddings, columns=embedding_columns)
+    df = pd.concat([df, embeddings_df], axis=1)
 
     # Save dataset locally
     today = date.today().strftime('%Y%m%d')
